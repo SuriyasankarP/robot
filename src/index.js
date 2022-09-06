3bcdf94aba04cb6a58f6641a508405b0a071401f
Suriya <suriyasankarp.tech@gmail.com>
2022-09-06 22:34:51 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "tachyons";
+import { robot } from './robot';
+import Card from './card';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+            <Card id={robot[0].id} name={robot[0].name} email={robot[0].email}/>
+            <Card id={robot[1].id} name={robot[1].name} email={robot[1].email}/>
+            <Card id={robot[2].id} name={robot[2].name} email={robot[2].email}/>
+            <Card id={robot[3].id} name={robot[3].name} email={robot[3].email}/>
+    </div>
+    
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+    
---
+++ src/robot.js
@@ -0,0 +1,27 @@
+
+export const robot=[{
+    id:1,
+    name:"Suriya",
+    email:"suriya@gmail.com"
+},
+{
+    id:2,
+    name:"Thavasi",
+    email:"Thavasi@gmail.com"
+},
+{
+    id:3,
+    name:"STR",
+    email:"str@gmail.com"
+},
+{
+    id:4,
+    name:"Vijay",
+    email:"vijay@gmail.com"
+},
+{
+    id:5,
+    name:"Varun",
+    email:"varun@gmail.com"
+}
+]
